6b13b441331398391b1a6ae347c912fba3e64ccc
Kattkovacs <kattkovacs@gmail.com>
2026-01-12 13:16:11 +0100

    add style changes

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,6 +8,7 @@ function LunchCheckController($scope, $filter) {
   LunchCheckController.$inject = ['$scope', '$filter'];
   $scope.numberOfItems = 0;
     $scope.listOfDishes = [];
+    $scope.textColor = 'black';
 
   $scope.checkListValue = function () {
     $scope.numberOfItems = $scope.listOfDishes.split(',').filter(function(item) {;
@@ -19,8 +20,10 @@ function LunchCheckController($scope, $filter) {
     if ($scope.numberOfItems === 0) {
       return "Please enter ingredients first";
     } else if ($scope.numberOfItems <= 3) {
+      $scope.textColor = 'green';
       return "Enjoy!";
     } else {
+      $scope.textColor = 'red';
       return "Too much!";
     }
   };
